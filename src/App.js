4b80ddebfbafd6de8cbc997e7f8b9b111abd8247
Cheolkie Kim <ckiekim@naver.com>
2020-03-07 15:41:21 +0900

    기본 틀

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
-import Dashboard from './dashboard/Dashboard';
-import SignIn from './templates/SignIn';
+/* import SignIn from './templates/SignIn';
 import SignUp from './templates/SignUp'; 
 import Album from './templates/Album';
 import Pricing from './templates/Pricing';
-import StickyFooter from './templates/StickyFooter';
+import StickyFooter from './templates/StickyFooter'; */
+import Dashboard from './dashboard/Dashboard';
+import MaterialUI from './templates/MaterialUI';
+import Main from './Main';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<Dashboard/>
+				<Main/>
 			</div>
 		);
 	}
